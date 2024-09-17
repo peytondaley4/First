1317ce2a602c464448b2b8a51d4a6288a1d10b33
peytondaley4 <95379090+peytondaley4@users.noreply.github.com>
2024-09-16 22:11:13 -0400

    Get p5 wrapper working with react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { ReactP5Wrapper } from 'react-p5-wrapper';
+import sketch from './sketch.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='App'>
+        <ReactP5Wrapper sketch={sketch}></ReactP5Wrapper>
+      </div>
+      <div>
+        Hi
+      </div>
     </div>
   );
 }
